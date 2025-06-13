7a2c9b4f46f8833f0ade35a6e6db0afd8101a405
orozaliev92 <orozalievvv@gmail.com>
2025-06-13 22:19:46 +0000

    homework-15

---
+++ Homework-15/task2.py
@@ -0,0 +1,8 @@
+foods=("Apple", "Banana", "Orange", "Mango", "Strawberry", "Grape", "Mandarin", "Strawberry")
+calories=(52, 96, 62, 605, 33, 68, 50, 33)
+
+foods_list=list(foods)
+calories_list=list(calories)
+
+print(f"5th elemenet is {foods_list[4]} with {calories_list[4]} calories")
+print(f"Second last food is {foods_list[-2]} with {calories_list[-2]} calories")
